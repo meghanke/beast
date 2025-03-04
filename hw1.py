19a1276fb11d433c4464027510246d536c59ca02
Meghan Ke <meghanke@visitor-10-56-168-57.wifi.berkeley.edu>
2025-03-03 16:22:49 -0800

    hi first commit

---
+++ hw1.py
@@ -0,0 +1,21 @@
+# Homework 1
+# pwd (print working directory)
+    # pwd displays the absolute path of the current working direction. use when you need to verify your current directory location in a terminal session. an example flag would be pwd -P. tihis prints the physical (real) path, resolving any symbolic links.
+# cd (change directory)
+    # cd swtiches from the current working directory to a different selected directory. use when you want to navigate to a different folder in the terminal. example flag: cd - switches back to the previous directory.
+# ls (list directory contents)
+    # ls lists files and directories in the current or specified directory. use to view avaliable files and subdirectories. example: ls -a lists all files, including hidden ones.
+# mkdir (make directory)
+    # mkdir makes a new directory. use to create a new folder in the file system. example flag: mkdir -p newfolder/subfolder to create parent directories as needed.
+# cp (copy files and directories)
+    # cp copies files or directories from one location to another. use it when duplicating files or making backups. example flag: cp -r folder1 folder2 copies a directory (folder1) and all contents recursively.
+# mv (move/rename files)
+    # mv noves or renames files and directories. use to rename a file or move it to another directory. example flag mv -i file1.txt file2.tct are prompts before overwriting if file2.txt already exists.
+
+
+# 1. git is a version control system which tracks changes in files and helps in collaborative software development. it was deceloped before github. github is a cloud-based hositng service for git repositories, allowing multiple developers to collaborate, review code, and manage projects using Git.
+# 2. the terminal line is an interface that allows users to interact with the system using a command-line shell. a command line is the text interface where you type commands.
+# 3. Windows PowerShell is a shell and scripting language built for Windows, while Git Bash is a Unix-like shell that comes with Git for Windows, provides Linux commands and Git functionality.
+# 4. Python is a programming language used for scripting, automation, and datasci. Condo is a package and enviroment manager that helps install Python and other dependencies. Anaconda is a Python distribution that includes Conda, Python, and pre-installed libraries for data science and machine learning. 
+# 5. A Jupyter Notebook is a web-based interactive tool for writing and executing Python code in "cells", widely used for data analysis, visualization, and machine learning. Jupyter Lab is an enhanced version of Jupyter notebook that provides multiple notebooks, terminals, and file management in one interface.
+# 6. VS vode is a lightweight open-source code editor developed by Microsoft, and it supports multiple programming languages and features.
